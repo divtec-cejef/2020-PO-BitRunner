2dd3993b2e85718a3db1454b0bec644e5b8e5d14
Matteo Membrez <54532942+membmat@users.noreply.github.com>
2020-06-04 14:43:12 +0200

    Add files via upload

---
+++ src/Assets/Script/Collision.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+
+    // Constantes
+    const float delay = 2;
+    const float lowSpeed = -1.0f;
+    const float basicSpeed = 5.0f;
+    const float highSpeed = 8.0f;
+
+    // Si le personnage entre en collision avec un obstacle ou un bonus
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject player = GameObject.Find("Player");
+
+        // Diminue la vitesse du personnage quand il rentre dans un obstacle
+        if (col.gameObject.tag == "Obstacle")
+        {
+            player.GetComponent<PlayerMovement>().Speed = lowSpeed;
+            StartCoroutine(ExecuteAfterTime(delay));
+        }
+
+        // Augmente la vitesse du personnage quand il rentre dans un bonus
+        if (col.gameObject.tag == "Bonus")
+        {
+            player.GetComponent<PlayerMovement>().Speed = highSpeed;
+            StartCoroutine(ExecuteAfterTime(delay));
+        }
+
+        // Permet d'effectuer du code après un délai
+        IEnumerator ExecuteAfterTime(float time)
+        {
+            yield return new WaitForSeconds(time);
+
+            player.GetComponent<PlayerMovement>().Speed = basicSpeed;
+        }
+    }
+}
